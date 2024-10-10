e696f4d58f77d6ab00eb58abf3c3ff208de1565c
Marco Borba <marcoborba1702@gmail.com>
2024-10-09 21:22:48 -0300

    Criação do Menu

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -28,19 +28,256 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.label9 = new System.Windows.Forms.Label();
+            this.lsbItensPedido = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnFecharPedido = new System.Windows.Forms.Button();
+            this.lblValorTotal = new System.Windows.Forms.Label();
+            this.lblQuantidadeItens = new System.Windows.Forms.Label();
+            this.btnNovoPedido = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(705, 11);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(297, 46);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Itens do Pedido";
+            // 
+            // lsbItensPedido
+            // 
+            this.lsbItensPedido.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lsbItensPedido.FormattingEnabled = true;
+            this.lsbItensPedido.ItemHeight = 20;
+            this.lsbItensPedido.Location = new System.Drawing.Point(517, 67);
+            this.lsbItensPedido.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lsbItensPedido.Name = "lsbItensPedido";
+            this.lsbItensPedido.Size = new System.Drawing.Size(662, 444);
+            this.lsbItensPedido.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(176, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 46);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Novo Pedido";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.btnFecharPedido);
+            this.panel1.Controls.Add(this.lblValorTotal);
+            this.panel1.Controls.Add(this.lblQuantidadeItens);
+            this.panel1.Controls.Add(this.btnNovoPedido);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtCpf);
+            this.panel1.Controls.Add(this.txtNome);
+            this.panel1.Location = new System.Drawing.Point(12, 67);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(499, 615);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnFecharPedido
+            // 
+            this.btnFecharPedido.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFecharPedido.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnFecharPedido.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFecharPedido.ForeColor = System.Drawing.Color.White;
+            this.btnFecharPedido.Location = new System.Drawing.Point(8, 561);
+            this.btnFecharPedido.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFecharPedido.Name = "btnFecharPedido";
+            this.btnFecharPedido.Size = new System.Drawing.Size(112, 50);
+            this.btnFecharPedido.TabIndex = 11;
+            this.btnFecharPedido.Text = "Fechar Pedido";
+            this.btnFecharPedido.UseVisualStyleBackColor = false;
+            // 
+            // lblValorTotal
+            // 
+            this.lblValorTotal.AutoSize = true;
+            this.lblValorTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorTotal.ForeColor = System.Drawing.Color.White;
+            this.lblValorTotal.Location = new System.Drawing.Point(123, 332);
+            this.lblValorTotal.Name = "lblValorTotal";
+            this.lblValorTotal.Size = new System.Drawing.Size(64, 25);
+            this.lblValorTotal.TabIndex = 4;
+            this.lblValorTotal.Text = "label2";
+            // 
+            // lblQuantidadeItens
+            // 
+            this.lblQuantidadeItens.AutoSize = true;
+            this.lblQuantidadeItens.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantidadeItens.ForeColor = System.Drawing.Color.White;
+            this.lblQuantidadeItens.Location = new System.Drawing.Point(173, 288);
+            this.lblQuantidadeItens.Name = "lblQuantidadeItens";
+            this.lblQuantidadeItens.Size = new System.Drawing.Size(64, 25);
+            this.lblQuantidadeItens.TabIndex = 4;
+            this.lblQuantidadeItens.Text = "label2";
+            // 
+            // btnNovoPedido
+            // 
+            this.btnNovoPedido.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnNovoPedido.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnNovoPedido.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNovoPedido.ForeColor = System.Drawing.Color.White;
+            this.btnNovoPedido.Location = new System.Drawing.Point(384, 561);
+            this.btnNovoPedido.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnNovoPedido.Name = "btnNovoPedido";
+            this.btnNovoPedido.Size = new System.Drawing.Size(112, 50);
+            this.btnNovoPedido.TabIndex = 3;
+            this.btnNovoPedido.Text = "Novo Pedido";
+            this.btnNovoPedido.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(83, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 46);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Detalhes";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(3, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(118, 25);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Valor Total: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(3, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Quantidade Itens: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "CPF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCpf.Location = new System.Drawing.Point(3, 129);
+            this.txtCpf.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(478, 35);
+            this.txtCpf.TabIndex = 0;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(3, 42);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(478, 35);
+            this.txtNome.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1013, 526);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 152);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
             // 
             // frmMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lsbItensPedido);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Chocolate;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMenu";
-            this.Text = "Menu";
+            this.Text = "Tela Pedidos";
+            this.Load += new System.EventHandler(this.frmMenu_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox lsbItensPedido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFecharPedido;
+        private System.Windows.Forms.Label lblValorTotal;
+        private System.Windows.Forms.Label lblQuantidadeItens;
+        private System.Windows.Forms.Button btnNovoPedido;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
